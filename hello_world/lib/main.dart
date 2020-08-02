a0114b0798e38996007943a69371c16dfe15b714
rahulguptafullstack <rahulguptafullstack@gmail.com>
2020-08-03 01:19:33 +0530

    lesson-7

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      title: 'My Flutter App',
-      home: Center(child: Text('My App')),
-      debugShowCheckedModeBanner: false,
-    ));
+    title: 'My Flutter App',
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Geeks Rank'),
+      ),
+      body: Center(
+        child: Text('Hello World'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('click'),
+        onPressed: () => {
+          print('You clicked me'),
+        },
+        tooltip: 'Click button',
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 40,
+          child: Text('Bottom Bar'),
+        ),
+      ),
+    )));
