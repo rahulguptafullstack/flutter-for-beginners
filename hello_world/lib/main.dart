931d667c198ef2ff34b1e77272de5ebb18d840df
rahulguptafullstack <rahulguptafullstack@gmail.com>
2020-08-03 02:24:37 +0530

    lesson-7

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -1,26 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-    title: 'My Flutter App',
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Geeks Rank'),
-      ),
-      body: Center(
-        child: Text('Hello World'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Text('click'),
-        onPressed: () => {
-          print('You clicked me '),
-        },
-        tooltip: 'Click button',
-      ),
-      bottomNavigationBar: BottomAppBar(
-        child: Container(
-          height: 40,
-          child: Text('Bottom Bar'),
-        ),
-      ),
-    )));
+      title: 'My Flutter App',
+      home: Text.rich(TextSpan(text: 'Welcome to', // default text style,
+          children: <TextSpan>[
+            TextSpan(
+                text: 'Geeks Rank',
+                style: TextStyle(fontStyle: FontStyle.italic, fontSize: 70.0)),
+            TextSpan(text: 'Youtube Channel', style: TextStyle(fontSize: 20.0))
+          ])),
+      debugShowCheckedModeBanner: false,
+    ));
