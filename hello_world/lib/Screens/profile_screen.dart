71be62d15822886f1c9724547b2d996cfa5de253
rahulguptafullstack <rahulguptafullstack@gmail.com>
2020-09-08 23:43:33 +0530

    lesson-30

--- hello_world/lib/Screens/profile_screen.dart
+++ hello_world/lib/Screens/profile_screen.dart
@@ -17,132 +17,137 @@ class _ProfileState extends State<Profile> {
       appBar: AppBar(
         title: Text('Profile'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                      hintText: 'Enter First Name',
-                      labelText: 'First Name',
-                      prefixIcon: Icon(
-                        Icons.person,
-                        color: Colors.grey,
-                      ),
-                      errorStyle: TextStyle(fontSize: 17.0),
-                      border: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey),
-                          borderRadius:
-                              BorderRadius.all(Radius.circular(30.0)))),
-                  // validator: (value) {
-                  //   if (value.isEmpty) {
-                  //     return "Please enter first name";
-                  //   } else if (value.length < 5) {
-                  //     return "Please enter atleast 5 characters";
-                  //   }
-                  //   return null;
-                  // },
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        hintText: 'Enter First Name',
+                        labelText: 'First Name',
+                        prefixIcon: Icon(
+                          Icons.person,
+                          color: Colors.grey,
+                        ),
+                        errorStyle: TextStyle(fontSize: 17.0),
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(30.0)))),
+                    // validator: (value) {
+                    //   if (value.isEmpty) {
+                    //     return "Please enter first name";
+                    //   } else if (value.length < 5) {
+                    //     return "Please enter atleast 5 characters";
+                    //   }
+                    //   return null;
+                    // },
 
-                  validator: MultiValidator([
-                    RequiredValidator(errorText: 'Please enter first name'),
-                    MinLengthValidator(5,
-                        errorText: 'First name should be atleast 5 characters')
-                  ]),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                      hintText: 'Enter Last Name',
-                      labelText: 'Last Name',
-                      prefixIcon: Icon(
-                        Icons.person,
-                        color: Colors.grey,
-                      ),
-                      errorStyle: TextStyle(fontSize: 17.0),
-                      border: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey),
-                          borderRadius:
-                              BorderRadius.all(Radius.circular(30.0)))),
-                  validator: MultiValidator([
-                    RequiredValidator(errorText: 'Please enter last name'),
-                    MinLengthValidator(5,
-                        errorText: 'Last name should be atleast 5 characters')
-                  ]),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                      hintText: 'Enter Email',
-                      labelText: 'Email',
-                      prefixIcon: Icon(
-                        Icons.email,
-                        color: Colors.grey,
-                      ),
-                      errorStyle: TextStyle(fontSize: 17.0),
-                      border: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey),
-                          borderRadius:
-                              BorderRadius.all(Radius.circular(30.0)))),
-                  validator: MultiValidator([
-                    RequiredValidator(errorText: 'Please enter email'),
-                    EmailValidator(errorText: 'Please enter a valid email')
-                  ]),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                      hintText: 'Enter Mobile',
-                      labelText: 'Mobile',
-                      prefixIcon: Icon(
-                        Icons.phone,
-                        color: Colors.grey,
-                      ),
-                      errorStyle: TextStyle(fontSize: 17.0),
-                      border: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey),
-                          borderRadius:
-                              BorderRadius.all(Radius.circular(30.0)))),
-                  validator: MultiValidator([
-                    RequiredValidator(errorText: 'Please enter mobile number'),
-                    PatternValidator(r'(^[0,9]{10}$)',
-                        errorText: 'Please enter valid mobile number')
-                  ]),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: 60.0,
-                  color: Colors.transparent,
-                  width: MediaQuery.of(context).size.width,
-                  child: RaisedButton(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30.0)),
-                    child: Text(
-                      'UPDATE',
-                      style: TextStyle(color: Colors.white, fontSize: 18.0),
-                    ),
-                    color: Colors.blue[500],
-                    onPressed: () {
-                      if (_formKey.currentState.validate()) {
-                        print('Form submitted');
-                      }
-                    },
+                    validator: MultiValidator([
+                      RequiredValidator(errorText: 'Please enter first name'),
+                      MinLengthValidator(5,
+                          errorText:
+                              'First name should be atleast 5 characters')
+                    ]),
                   ),
                 ),
-              )
-            ],
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        hintText: 'Enter Last Name',
+                        labelText: 'Last Name',
+                        prefixIcon: Icon(
+                          Icons.person,
+                          color: Colors.grey,
+                        ),
+                        errorStyle: TextStyle(fontSize: 17.0),
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(30.0)))),
+                    validator: MultiValidator([
+                      RequiredValidator(errorText: 'Please enter last name'),
+                      MinLengthValidator(5,
+                          errorText: 'Last name should be atleast 5 characters')
+                    ]),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        hintText: 'Enter Email',
+                        labelText: 'Email',
+                        prefixIcon: Icon(
+                          Icons.email,
+                          color: Colors.grey,
+                        ),
+                        errorStyle: TextStyle(fontSize: 17.0),
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(30.0)))),
+                    validator: MultiValidator([
+                      RequiredValidator(errorText: 'Please enter email'),
+                      EmailValidator(errorText: 'Please enter a valid email')
+                    ]),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        hintText: 'Enter Mobile',
+                        labelText: 'Mobile',
+                        prefixIcon: Icon(
+                          Icons.phone,
+                          color: Colors.grey,
+                        ),
+                        errorStyle: TextStyle(fontSize: 17.0),
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(30.0)))),
+                    validator: MultiValidator([
+                      RequiredValidator(
+                          errorText: 'Please enter mobile number'),
+                      PatternValidator(r'(^[0,9]{10}$)',
+                          errorText: 'Please enter valid mobile number')
+                    ]),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 60.0,
+                    color: Colors.transparent,
+                    width: MediaQuery.of(context).size.width,
+                    child: RaisedButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0)),
+                      child: Text(
+                        'UPDATE',
+                        style: TextStyle(color: Colors.white, fontSize: 18.0),
+                      ),
+                      color: Colors.blue[500],
+                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+                          print('Form submitted');
+                        }
+                      },
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
